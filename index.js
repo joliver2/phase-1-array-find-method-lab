820754c21bfe2cc0964787474d1fea4adab14d57
Jeremy Oliver <joliver427@icloud.com>
2022-12-26 14:11:03 -0700

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function superbowlWin(array) {
+    const win = array.find( function(game) {
+        if (game.result === 'W') {
+            return game.year;
+        }
+    })
+    if (win) {
+        return win.year;
+    }
+    else {
+        return undefined;
+    }
+}
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+console.log(superbowlWin(record))
